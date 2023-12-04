338fba7da931a73f8f97461ef98ea90c9b4a8568
jayjader <jayjader@users.noreply.github.com>
2023-12-04 10:08:40 +0100

    extract parsing

--- src/main.rs
+++ src/main.rs
@@ -542,7 +542,15 @@ mod day3 {
 }
 
 mod day4 {
-    use std::collections::HashMap;
+    use std::collections::{BinaryHeap, HashMap};
+
+    fn parse_numbers(winning: &str) -> impl Iterator<Item = u8> + '_ {
+        winning
+            .trim()
+            .split(' ')
+            .filter(|chars| !chars.is_empty())
+            .map(|n| n.parse::<u8>().unwrap())
+    }
 
     pub fn part1(input: &str) -> usize {
         input
@@ -551,21 +559,10 @@ mod day4 {
             .map(|line| {
                 let (_, rest) = line.split_once(':').unwrap();
                 let (winning, drawn) = rest.split_once('|').unwrap();
-                let winning: Vec<_> = winning
-                    .trim()
-                    .split(' ')
-                    .filter(|chars| !chars.is_empty())
-                    .map(|n| n.parse::<u8>().unwrap())
-                    .collect();
-                let drawn: Vec<_> = drawn
-                    .trim()
-                    .split(' ')
-                    .filter(|chars| !chars.is_empty())
-                    .map(|n| n.parse::<u8>().unwrap())
-                    .collect();
+                let winning = parse_numbers(winning).collect::<BinaryHeap<u8>>();
+                let drawn = parse_numbers(drawn);
                 let scoring = drawn
-                    .iter()
-                    .filter(|number| winning.contains(number))
+                    .filter(|number| winning.iter().any(|w| w == number))
                     .count();
                 if scoring == 0 {
                     0
@@ -587,54 +584,37 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         assert_eq!(part1(input), 13)
     }
     pub fn part2(input: &str) -> usize {
-        #[derive(Default)]
-        struct Card {
-            winning: Vec<u8>,
-            drawn: Vec<u8>,
-        }
         input
             .split('\n')
             .filter(|line| !line.is_empty())
             .enumerate()
             .fold(HashMap::new(), |mut accum, (card_index, line)| {
                 let card_num = card_index + 1;
-                if (accum.get(&card_num).is_none()) {
+                if accum.get(&card_num).is_none() {
                     accum.insert(card_num, 0);
                 }
                 let new_count = accum.get(&card_num).unwrap() + 1;
                 accum.insert(card_num, new_count);
                 let (_, rest) = line.split_once(':').unwrap();
                 let (winning, drawn) = rest.split_once('|').unwrap();
-                let winning: Vec<_> = winning
-                    .trim()
-                    .split(' ')
-                    .filter(|chars| !chars.is_empty())
-                    .map(|n| n.parse::<u8>().unwrap())
-                    .collect();
-                let drawn: Vec<_> = drawn
-                    .trim()
-                    .split(' ')
-                    .filter(|chars| !chars.is_empty())
-                    .map(|n| n.parse::<u8>().unwrap())
-                    .collect();
+                let winning = parse_numbers(winning).collect::<BinaryHeap<u8>>();
+                let drawn = parse_numbers(drawn);
                 let matches = drawn
-                    .iter()
-                    .filter(|number| winning.contains(number))
+                    .filter(|number| winning.iter().any(|w| w == number))
                     .count();
-                if matches > 0 {
-                    for offset in 1..=matches {
-                        let copy_num = card_num + offset;
-                        accum.insert(
-                            copy_num,
-                            accum.get(&copy_num).unwrap_or(&0usize) + new_count,
-                        );
-                    }
+                for offset in 1..=matches {
+                    let copy_num = card_num + offset;
+                    accum.insert(
+                        copy_num,
+                        accum.get(&copy_num).unwrap_or(&0usize) + new_count,
+                    );
                 }
                 accum
             })
             .values()
             .sum()
     }
+
     #[test]
     fn part2_on_sample() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
